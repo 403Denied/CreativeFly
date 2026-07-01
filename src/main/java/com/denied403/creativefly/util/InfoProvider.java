910afff1b3a113e3a5e9d8bb9406aae6d870427c
403Denied <66492638+403Denied@users.noreply.github.com>
2026-07-01 18:40:55 -0500

    Initial commit

---
+++ src/main/java/com/denied403/creativefly/util/InfoProvider.java
@@ -0,0 +1,7 @@
+package com.denied403.creativefly.util;
+
+import net.minecraft.client.Minecraft;
+
+public class InfoProvider {
+    private static final Minecraft client = Minecraft.getInstance();
+}
